57da162cac7929ab06c4be08528ffee42b22f575
sharukhan <sharukhan@webkorps.com>
2024-07-11 23:20:17 +0530

    added routes

--- src/app/MyComponents/todo-items/todo-items.component.ts
+++ src/app/MyComponents/todo-items/todo-items.component.ts
@@ -12,21 +12,28 @@ import { AddTodoComponent } from '../add-todo/add-todo.component';
   styleUrl: './todo-items.component.css'
 })
 export class TodoItemsComponent {
+  localTodos: string | null;
   todos: Todo[]
 
   constructor() {
-    this.todos = [
-      { id: 1, title: 'learn html' }, { id: 2, title: 'learn css' }, { id: 3, title: 'learn js' }
-    ]
+    this.todos = []
+    setTimeout(()=>{
+      this.localTodos = localStorage.getItem('todos');
+      if(this.localTodos === null){
+        this.todos = []
+      }else{
+        this.todos = JSON.parse(this.localTodos)
+      }
+    },1000)
   }
 
   deleteTodo(todo: Todo) {
-    console.log(todo)
     this.todos = this.todos.filter((td) => td.id !== todo.id)
+    localStorage.setItem('todos', JSON.stringify(this.todos))
   }
 
   addTodo(todo: Todo) {
-    console.log(todo)
     this.todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(this.todos))
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoItemsComponent } from './MyComponents/todo-items/todo-items.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: TodoItemsComponent},
+    // {path: 'search', component: AddTodoComponent}
+];
